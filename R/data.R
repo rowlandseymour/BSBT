cf35ecfca0e875d1d34ed74551f87794172a3aee
Rowland Seymour <rowland@Rowlands-MBP.lan>
2020-06-24 10:56:49 +0100

    removed reference to raw data

--- R/data.R
+++ R/data.R
@@ -6,13 +6,10 @@
 #' The data was collected in August 2018.
 #' @docType data
 #'
-#' @usage system.file("extdata", "dar.csv", package = "BTUN")
 #'
 #' @format A csv file containing 76408 rows and 2 columns. Each row corresponds to a judgement made by a single judge. The column shows which of the pair of subwards was judged to be poorest and richest.
 #'
 #' @keywords datasets
-#'
-#' @usage system.file("extdata", "dar.csv", package = "BTUN")
 #'
 #'@source This dataset was collected by Madeleine Ellis, James Goulding, Bertrand Perrat, Gavin Smith and Gregor Engelmann.We gratefully acknowledge the Rights Lab at the University of Nottingham for supporting funding for the comprehensive ground truth survey. We also acknowledge HumanitarianStreet Mapping Team (HOT) for providing a team of experts in data collectionto facilitate the surveys. This work was also supported by the EPSRC HorizonCentre for Doctoral Training - My Life in Data (EP/L015463/1) and EPSRC grant Neodemographics (EP/L021080/1).
 #'
@@ -28,11 +25,9 @@
 #'
 #' @docType data
 #'
-#' @usage system.file("extdata", "dar_subwards.shp", package = "BTUN")
 #'
 #' @format A .shp object
 #'
-#' @usage system.file("extdata", "dar_subwards.shp", package = "BTUN")
 #'
 #' @keywords datasets
 #'
