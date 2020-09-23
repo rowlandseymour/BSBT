84a43da6e38870028a569268102a310204dcbcb0
Rowland Guy Seymour <pmars3@imac-dgkzvhurjv3x.lan>
2020-09-23 15:34:03 +0100

    Remove level 1 heading

--- R/simulate_contests.R
+++ R/simulate_contests.R
@@ -2,13 +2,12 @@
 #'
 #' This function simulates pair-wise contests according to the Bradley--Terry model. It require the true quality of the areas and the number of comparisons to be carried out. It can also include some judge noise or error.
 #' When including noise, each time a judge carries out a comparisons, we assume they use the true quality with some zero-mean normal noise added. The standard deviation must be specified.
-
 #'
 #' @param n.contests The number of contests to be carried out
 #' @param true.quality A vector with the level of deprivation in each area on the log scale.
-#' @param sigma.obs Standard deviation for the noise to be added to the level of deprivation in each subward. If 0, no noise is used.
+#' @param sigma.obs Standard deviation for the noise to be added to the level of deprivation in each area. If 0, no noise is used.
 #' @return A list containing a data.frame with each par-wise contest, the outcome (a 1 for a win, a 0 for a loss),
-#' # and a win matrix where the i,j^th element is the number of times i beat j
+#'  and a win matrix where the i,j^th element is the number of times i beat j
 #'
 #' @examples
 #'
