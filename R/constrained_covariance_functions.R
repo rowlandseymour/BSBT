bf2ea6ef3c1c8cb67a5ac9b3bddeae497f9e3f2d
Rowland Seymour <rowland.seymour@nottingham.ac.uk>
2020-10-15 13:23:17 +0100

    Switch to SD decomposition of covariance matrix

--- R/constrained_covariance_functions.R
+++ R/constrained_covariance_functions.R
@@ -14,11 +14,11 @@
 #'
 #' @examples
 #' #Generate 10 points and create covariance matrix using Euclidean distance metric
-#' coords <- data.frame("x" = runif(10), "y" = runif(10)) #generate coordinates
+#' coords <- data.frame("x" = c(0, 1, 2), "y" = c(0, 1, 2)) #generate coordinates
 #' #create covariance matrix using Squared Exponential function and subject to the constraint
 #' #the sum of the deprivation levels is 0.
 #' k <- constrained_covariance_function(coords, "sqexp",
-#' c(1, 0.5), rep(1, 10), linear.constraint = 0)
+#' c(1, 5), rep(1, 3), linear.constraint = 0)
 #' @export
 constrained_covariance_function <- function(coordinates, type, hyperparameters,
                                            linear.combination, linear.constraint = 0){
@@ -60,16 +60,18 @@ constrained_covariance_function <- function(coordinates, type, hyperparameters,
   constrained.k             <- k - k%*%linear.combination%*%t(k%*%linear.combination)*as.numeric((1/(linear.combination%*%k%*%linear.combination)))
   constrained.mean          <- prior.mean + as.numeric((0-t(prior.mean)%*%linear.combination)/(t(linear.combination)%*%k%*%linear.combination))*k%*%linear.combination
   spectral.decomp           <- eigen(constrained.k)
-  constrained.decomp            <- spectral.decomp$vectors%*%diag(sqrt(spectral.decomp$values))
+  spectral.decomp$values[spectral.decomp$values < 0] <- 0 #Deal with precision errors in covariance matrix
+  constrained.decomp        <- spectral.decomp$vectors%*%diag(sqrt(spectral.decomp$values))
+  constrained.inv           <- MASS::ginv(constrained.k)
 
- return(list("mean" = constrained.mean, "covariance" = constrained.k, "decomp.covariance" = constrained.decomp))
+ return(list("mean" = constrained.mean, "covariance" = constrained.k, "decomp" = constrained.decomp, "inv" = constrained.inv))
 }
 
 
 
-#' Construct a constrained covariance matrix from the adjaency matrix
+#' Construct a constrained covariance matrix from the adjacency matrix
 #'
-#' This function constructs a covariance function from the graph's adjacency matrix. The covairance function may be squared exponential, rational quadratic or Matern. It includes a constraint, where a linear combination of the parameters can be fixed.
+#' This function constructs a covariance function from the graph's adjacency matrix. The covariance function may be squared exponential, rational quadratic or Matern. It includes a constraint, where a linear combination of the parameters can be fixed.
 #'
 #'
 #' @param adj.matrix The graph adjacency matrix
@@ -85,10 +87,8 @@ constrained_covariance_function <- function(coordinates, type, hyperparameters,
 #' #Construct covariance matrix of Dar es Salaam, Tanzania, using network metric
 #' data(dar.adj.matrix, package = "BSBT") #load dar es salaam adjacency matrix
 #' k <- constrained_adjacency_covariance_function(dar.adj.matrix, type = "sqexp",
-#'        hyperparameters = c(1, 0.5), rep(1, dim(dar.adj.matrix)[1]), 0)
+#'        hyperparameters = c(1, 1), rep(1, dim(dar.adj.matrix)[1]), 0)
 #'        #Covariance registetred by sum of subwards is 0 using rational quadratic function
-#' k <- constrained_adjacency_covariance_function(dar.adj.matrix, type = "ratquad",
-#'        hyperparameters = c(1, 0.5, 2), rep(1, dim(dar.adj.matrix)[1]), 0)
 #' @export
 constrained_adjacency_covariance_function <- function(adj.matrix, type, hyperparameters,
                                                      linear.combination, linear.constraint = 0){
@@ -116,6 +116,7 @@ constrained_adjacency_covariance_function <- function(adj.matrix, type, hyperpar
   #Construct Covariance Matrix
   if(type == "sqexp"){
     k <- hyperparameters[1]^2*exp(-shortest.path.matrix^2/hyperparameters[2]^2)
+
   } else if(type == "ratquad"){
     k <- hyperparameters[1]^2*(1 + shortest.path.matrix^2/(2*(hyperparameters[2]^2)*hyperparameters[3]))^(-hyperparameters[3])
   } else if(type == "matern"){
@@ -133,9 +134,11 @@ constrained_adjacency_covariance_function <- function(adj.matrix, type, hyperpar
   constrained.k             <- k - k%*%linear.combination%*%t(k%*%linear.combination)*as.numeric((1/(linear.combination%*%k%*%linear.combination)))
   constrained.mean          <- prior.mean + as.numeric((0-t(prior.mean)%*%linear.combination)/(t(linear.combination)%*%k%*%linear.combination))*k%*%linear.combination
   spectral.decomp           <- eigen(constrained.k)
+  spectral.decomp$values[spectral.decomp$values < 0] <- 0 #Deal with precision errors in covariance matrix
   constrained.decomp        <- spectral.decomp$vectors%*%diag(sqrt(spectral.decomp$values))
+  constrained.inv           <- MASS::ginv(constrained.k)
 
-  return(list("mean" = constrained.mean, "covariance" = constrained.k, "decomp.covariance" = constrained.decomp))
+  return(list("mean" = constrained.mean, "covariance" = constrained.k, "decomp" = constrained.decomp, "inv" = constrained.inv))
 }
 
 
