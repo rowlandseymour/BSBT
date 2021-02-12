b264d076ecbb962544e4547d5526ce1da7a085ab
Rowland Seymour <rowland.seymour@nottingham.ac.uk>
2021-02-12 08:44:25 +0000

    Include test for matrix exponential

--- tests/testthat/test-covariance.R
+++ tests/testthat/test-covariance.R
@@ -11,6 +11,9 @@ test_that("constrained adjacent covariance works", {
   expect_error(constrained_adjacency_covariance_function(dar.adj.matrix, type = "sqexp", hyperparameters = c(1, 0.5),
                                                         linear.combination = rep(1, 41),
                                                         linear.constraint = 0), "Could not constrain distirbution. Linear constraint dimensions does not match number of objects.")
+  expect_error(constrained_adjacency_covariance_function(dar.adj.matrix, type = "matrix", hyperparameters = c(1, 0.5),
+                                                         linear.combination = rep(1, 41),
+                                                         linear.constraint = 0), "Insufficient hyperparameters. Matrix exponential requires 1 value.")
 })
 
 coords <- data.frame("x" = runif(10), "y" = runif(10))
